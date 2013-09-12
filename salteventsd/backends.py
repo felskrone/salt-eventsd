1ad0d405085d880cfbfe43f39baec9a805da8fd3
Volker Schwicking <vs@hosteurope.de>
2013-09-12 12:40:19 +0000

    updated documentation

--- salteventsd/backends.py
+++ salteventsd/backends.py
@@ -7,6 +7,10 @@ import logging
 log = logging.getLogger(__name__)
 
 class BackendMngr(object):
+    '''
+    Loads classes from python-files named "^[aA-zZ]+Worker.py$" from the 
+    backends-directory (default: /etc/salt/eventsd_workers)
+    '''
 
     def __init__(self, be_dirs):
         self.be_dirs = be_dirs
@@ -15,49 +19,57 @@ class BackendMngr(object):
         
 
     def listWorkers(self, directory):
+	'''
+	lists files matching the naming_schema of the workers and loads 
+	the class from them by trying to load a class named 
+	<filename_without_extension> from it
+	'''
         for root, subFolders, files in os.walk(directory):
             for filename in files:
                 if( re.match(self.naming_scheme, filename) ):
                     new_plugin = self.loadPluginFromFile(filename)
 
+
     def loadPlugins(self):
+	'''
+	wrapper-function for listWorkers()
+	'''
         for path in self.be_dirs:
             self.listWorkers(path)
         return self.backends
         
 
     def loadPluginFromFile(self, filename):    
+	'''
+	loads a class from a file name by trying to load a class
+	named like the given filename without extension
+	'''
         (path, name) = os.path.split(filename)
         (name, ext) = os.path.splitext(name)
 
-        # if we try to load a module from file, the module might be using import to import other libraries, for example
-        # a WebpackLib.py. since we import a plugin by full-path-name which is usually NOT in the python-search-path,
-        # a library would have to reside in the same directory as the plugin to be found. 
-        # since we dont want various library-files (which essentially might do the same thing) scattered throughout the 
-        # plugin-directories and certainly not in a python-search-path that has nothing to with the webpack-shell (like /usr/lib/python2.x)
-        # we define one library-dir for job-plugins which we add to the python-search-path. if a job-plugin uses import
-        # statements and trys to import a library, only the std-python-search-path, the plugins cwd and this library-path
-        # are searched for that imported module. if it cant be found, the plugin is skipped
+	# insert the paths defined in the be_dirs into our path-variable
         for path in self.be_dirs:
             sys.path.insert(0, path)
 
-        # look in the current search path for given module_name
+        # look in the current search path for the given module_name
         try:
             (file, filename, data) = imp.find_module(name, [path] )
             # load the module from file
             loaded_mod = imp.load_module(name, file, filename, data)
             log.info("loaded_mod: " + str(loaded_mod) )
 
-            # make sure the expected class is found in the loaded file. this means the name of the file
-            # we are importing must match the class-name in the file that is being imported (without .py). 
-            # otherwise we would not know which class to import from the file. it may also contain various
-            # other classes we dont need to know about
+            # make sure the expected class is found in the loaded file. this 
+	    # means the name of the file we are importing must match the class-
+	    # name in the file that is being imported (without .py). otherwise 
+	    # we would not know which class to import from the file. it may 
+	    # also contain various other classes we dont need to know about
             if( hasattr(loaded_mod, name) ):
                 plugin_class = getattr(loaded_mod, name)()
                 log.info(plugin_class)
                 self.backends[name] = plugin_class
             else:
-                log.info("ERR0: file found but not loaded {0}".format(filename))
+                log.error("ERR0: file '{0}' found but class '{1}' not found in" + \
+		          "file.".format(filename, name))
 
         except ImportError, i:
             log.exception(i)
