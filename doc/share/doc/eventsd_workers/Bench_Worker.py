b536676f427bfb8cc9767dea14fff83598f05b77
Grokzen <Grokzen@gmail.com>
2015-02-09 12:37:45 +0100

    Cleanup Bench_Worker.py

--- doc/share/doc/eventsd_workers/Bench_Worker.py
+++ doc/share/doc/eventsd_workers/Bench_Worker.py
@@ -1,14 +1,7 @@
-'''
-this Worker takes care of return-data being returned by a minion.
-it takes care of dumping the 'return'-field into the database
-after its json-encoded and base64 converted.
-'''
+# coding=utf-8
 
-# import and setup logger first, so we can catch
-# missing libraries on imports
+# python std lib
 import logging
-import requests
-import json
 
 log = logging.getLogger(__name__)
 
@@ -17,9 +10,6 @@ class Bench_Worker(object):
     '''
     takes care of dumping 'return'-data into the database
     '''
-    # the settings for the mysql-server to use
-    creds = {}
-
     name = "Bench_Worker"
     thread_id = None
 
@@ -28,9 +18,6 @@ class Bench_Worker(object):
         init the connection and do other necessary stuff
         '''
         self.thread_id = thread_id
-
-        # keep track of the events dumped
-        self.dumped = 0
 
         log.info("{0}# Worker '{1}' initiated".format(
             self.thread_id,
@@ -50,12 +37,5 @@ class Bench_Worker(object):
         It receives events with their corresponding setting and
         passes it on to a handler-function
         '''
-        # print("")
-
-        # print(entry)
-        # print("")
-        # print(event_set)
-
-        # print("")
-
-        pass
+        log.debug(entry)
+        log.debug(event_set)
